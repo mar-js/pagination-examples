b6b5c60561044b21c6731ea5b344fca8cded7978
Martin <maguera37@gmail.com>
2024-01-04 23:55:35 -0300

    feat: add ref in ul element

--- src/components/first-paginations/index.tsx
+++ src/components/first-paginations/index.tsx
@@ -1,16 +1,18 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
 import { generatorPages } from "../../utils";
 
 export const FirstPaginations: FC = () => {
 	const totalPages = 10;
 	const pages = generatorPages(totalPages);
 
+	const refPages = useRef(null);
+
 	return (
 		<div className="first-paginations">
 			<button className="arrow-left" type="button">
 				{"<"}
 			</button>
-			<ul className="pages">
+			<ul ref={refPages} className="pages">
 				{pages.map((page) => (
 					<li className="page" key={page}>
 						{page}
