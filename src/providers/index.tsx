f47ae8d02e6db0d3e1441df604ba03069ded049a
Martin <maguera37@gmail.com>
2024-01-11 21:58:48 -0300

    feat: create paginations provider component

---
+++ src/providers/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { PaginationsModel } from "../contexts";
+
+export const PaginationsProvider: FC = () => {
+	return (
+		<PaginationsModel.Provider value="">
+			asd
+		</PaginationsModel.Provider>
+	);
+};
