7eb86f86e21fe011d4ff047877656c26152679cd
Martin <maguera37@gmail.com>
2024-01-11 22:00:15 -0300

    feat: add paginations provider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { FC } from "react";
 import { FirstPaginations } from "./components";
+import { PaginationsProvider } from "./providers";
 
 export const App: FC = () => (
-	<>
+	<PaginationsProvider>
 		<FirstPaginations />
-	</>
+	</PaginationsProvider>
 );
