277911ddd029f6713bfb1525e1753c000199cf50
Martin <maguera37@gmail.com>
2024-01-11 22:11:03 -0300

    feat: create and add value const to provider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,11 +1,20 @@
 import { FC, PropsWithChildren } from "react";
-import { PaginationsModel } from "../contexts";
+import {
+	InterfacePaginationsModel,
+	PaginationsModel,
+} from "../contexts";
 
 export const PaginationsProvider: FC<PropsWithChildren> = ({
 	children,
 }) => {
+	const value: InterfacePaginationsModel = {
+		firstPaginations: {
+			numberPage: 0,
+			totalPages: 10,
+		},
+	};
 	return (
-		<PaginationsModel.Provider value="">
+		<PaginationsModel.Provider value={value}>
 			{children}
 		</PaginationsModel.Provider>
 	);
