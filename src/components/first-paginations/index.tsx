4a50bba22c84b5235e1763f6cae9b10984daa156
Martin <maguera37@gmail.com>
2024-01-11 22:11:24 -0300

    feat: add usePaginationsContext to FirstPaginations component

--- src/components/first-paginations/index.tsx
+++ src/components/first-paginations/index.tsx
@@ -1,11 +1,14 @@
 import { FC } from "react";
+import { usePaginationsContext } from "../../contexts";
 import { Paginations } from "./paginations";
 
 export const FirstPaginations: FC = () => {
+	const { firstPaginations } = usePaginationsContext();
+
 	return (
 		<section>
 			'AAA'
-			<Paginations />
+			<Paginations {...firstPaginations} />
 		</section>
 	);
 };
