2dfccad47be45a94410942cc53dea755c0d69df9
Martin <maguera37@gmail.com>
2024-01-14 22:23:00 -0300

    feat: add logic handleClickFirstPaginations

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,4 +1,4 @@
-import { FC, PropsWithChildren } from "react";
+import { FC, MouseEvent, PropsWithChildren, useState } from "react";
 import {
 	InterfacePaginationsModel,
 	PaginationsModel,
@@ -7,12 +7,53 @@ import {
 export const PaginationsProvider: FC<PropsWithChildren> = ({
 	children,
 }) => {
+	const [firstPaginations, setFirstPaginations] = useState({
+		numberPage: 0,
+		totalPages: 10,
+	});
+
+	const handleClickFirstPaginations = (
+		e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>,
+		liElements: HTMLCollection | undefined,
+	) => {
+		const btn = e.target as HTMLButtonElement;
+
+		if (!liElements) return;
+
+		if (btn.name === "btn-right") {
+			if (firstPaginations.numberPage === firstPaginations.totalPages)
+				return;
+
+			setFirstPaginations((prev) => ({
+				...prev,
+				numberPage: firstPaginations.numberPage + 1,
+			}));
+
+			liElements[firstPaginations.numberPage].classList.add("active");
+		} else {
+			if (firstPaginations.numberPage === 0) return;
+
+			setFirstPaginations((prev) => ({
+				...prev,
+				numberPage: firstPaginations.numberPage - 1,
+			}));
+
+			if (firstPaginations.numberPage === firstPaginations.totalPages)
+				return;
+
+			liElements[firstPaginations.numberPage].classList.remove(
+				"active",
+			);
+		}
+	};
+
 	const value: InterfacePaginationsModel = {
 		firstPaginations: {
-			numberPage: 0,
-			totalPages: 10,
+			...firstPaginations,
+			handleClick: handleClickFirstPaginations,
 		},
 	};
+
 	return (
 		<PaginationsModel.Provider value={value}>
 			{children}
