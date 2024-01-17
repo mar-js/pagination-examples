92e9a8427e821635f0015e474cec778a781b78d7
Martin <maguera37@gmail.com>
2024-01-17 20:23:00 -0300

    feat: add title component to first-paginations component

--- src/components/first-paginations/index.tsx
+++ src/components/first-paginations/index.tsx
@@ -1,13 +1,14 @@
 import { FC } from "react";
 import { usePaginationsContext } from "../../contexts";
 import { Paginations } from "./paginations";
+import { Title } from "./title";
 
 export const FirstPaginations: FC = () => {
 	const { firstPaginations } = usePaginationsContext();
 
 	return (
 		<section>
-			{firstPaginations.numberPage}
+			<Title>{firstPaginations.numberPage}</Title>
 			<Paginations {...firstPaginations} />
 		</section>
 	);
